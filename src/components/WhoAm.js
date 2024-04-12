0c75134b0ea7085c34454eb34d04a9a68704ff11
karpagavallis <karpagam28995@gmail.com>
2024-04-12 16:31:45 +0530

    Name change

--- src/components/WhoAm.js
+++ src/components/WhoAm.js
@@ -8,7 +8,7 @@ function WhoAm() {
                     <div className="offset-1 col-lg-3 portfolio-bg animate__animated animate__fadeInDown"></div>
                     <div className=" col-lg-7">
                         <div class="hero-content-box animate__animated animate__fadeInRight">
-                            <span className="hero-sub-title">I am Karpagavalli</span>
+                            <span className="hero-sub-title">I am Karpagavalli Santharam</span>
                             <h1 className="hero-title">Senior Engineer</h1>
                             <p className="hero-para">As a Senior Engineer, I specialize in crafting intuitive user interfaces that seamlessly blend aesthetics with functionality. </p>
                             <p className="hero-para">With a keen eye for design principles and a commitment to delivering engaging digital experiences, I strive to create interfaces that captivate users and enhance overall usability.</p>
